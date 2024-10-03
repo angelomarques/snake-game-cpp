ad5c0e51b0e80481979c42e0ffe8bfcec8c10d2a
Ângelo Marques <angeloemanuelmarques@gmail.com>
2024-10-03 20:33:35 -0300

    refactor: create colors class

---
+++ src/colors.cpp
@@ -0,0 +1,6 @@
+#include "colors.hpp"
+
+Colors::Colors() {}
+
+glm::vec3 Colors::light_gray = glm::vec3(0.8f, 0.8f, 0.8f);
+glm::vec3 Colors::medium_gray = glm::vec3(0.5f, 0.5f, 0.5f);
---
+++ src/colors.hpp
@@ -0,0 +1,19 @@
+#ifndef COLORS_HPP
+#define COLORS_HPP
+
+// #include <GL/glew.h>
+// #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+// #include <glm/gtc/matrix_transform.hpp>
+// #include <glm/gtc/type_ptr.hpp>
+
+class Colors
+{
+public:
+    Colors();
+
+    static glm::vec3 light_gray;
+    static glm::vec3 medium_gray;
+};
+
+#endif // COLORS_HPP
